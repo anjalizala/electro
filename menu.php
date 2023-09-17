68f521af6d357637d1b27261f6401653c272e375
Dhvani-13 <mistrydhvani927@gmail.com>
2023-09-18 00:54:01 +0530

    d/product

---
+++ menu.php
@@ -0,0 +1,30 @@
+<?php include "dbname.php";?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link type="text/css" rel="stylesheet" href="css/slick.css"/>
+        <link type="text/css" rel="stylesheet" href="css/slick-theme.css"/>
+</head>
+<body>
+<div class="products-slick" data-nav="#slick-nav-1">
+										<!-- product -->
+										<div class="product">
+											<div class="product-img">
+												<img src="./img/samsung.jpg" alt="">
+												<div class="product-label">
+													<!--<span class="sale">-30%</span>-->
+													<span class="new">NEW</span>
+                                                    <div class="product-body">
+												<p class="product-category"></p>
+												<h3 class="product-name"><a href="#"></a></h3>
+												<h4 class="product-price"> </h4>
+												</div>
+                                            </div>
+                                        </div>
+                                        </div>
+</div>                                       
+</body>
+</html>
---
+++ phn.php
@@ -0,0 +1,73 @@
+<?php include "header.php";?>
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<div class="product-body">
+        <h2>Led</h2>
+        <br><br>
+</div>
+<?php include "menu.php";?>      
+<?php 
+            include "dbname.php";
+            //Display all the laptop that are active
+            //Sql Query
+            $sql = "SELECT * FROM  phone";
+
+            //Execute the Query
+            $res = mysqli_query($conn, $sql);
+
+            //Count Rows
+            $count = mysqli_num_rows($res);
+             //CHeck whether categories available or not
+             if($count>0)
+             {
+                 //CAtegories Available
+                 while($row=mysqli_fetch_assoc($res))
+                 {
+                    echo "  Name: " . $row["name"]. " " . $row["model"]. " " . $row["price"]. "<br>";
+                     //Get the Values
+                   
+                    //  $name = $row['name']; 
+                    //  $model = $row['model']; 
+                    //  $price = $row['price'];
+                    // $img = $row['img']                  
+                     ?>
+                     
+                    <!--<a href="<?php //echo SITEURL; ?>product.php">-->
+                        <!-- <div class="box-3 float-container">
+                              <?php 
+                                //  if($img=="")
+                                //  {
+                                //      //Image not Available
+                                //      echo "<div class='error'>Image not found.</div>";
+                                //  }
+                                //  else
+                                //  {
+                                //      //Image Available
+                                //      ?>
+                                //      <img src="<?php //echo SITEURL; ?>img<?php // $img; ?>" alt="Pizza" class="img-responsive img-curve">
+                                //      <?php
+                                //  } 
+                             ?> 
+                             
+ 
+                             <h3 class="float-text text-white"><?php //echo $name; ?></h3>
+                         </div>-->
+                   <!-- </a>-->
+ 
+                     <?php
+                 }
+             }
+               //CAtegories Not Available
+                 else {
+                    echo "0 results";
+                  }
+                  
+                  mysqli_close($conn);
+         
+         ?>
+
+</body>
+</html> 
+
+<?php include "footer.php";?>
